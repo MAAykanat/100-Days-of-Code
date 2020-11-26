e770bae5f999ede2c828c1916bd1cea937226369
Muhammet Ali Aykanat <72671594+MAAykanat@users.noreply.github.com>
2020-11-27 00:23:46 +0300

    v1.0
    This is the first task to challenge

---
+++ Day_1/Day_1.py
@@ -0,0 +1,16 @@
+#Day Number: 1
+#Day Level: Beginner
+#The topic: Working with Variables in Python to Manage Data
+
+#The DUTY!!!
+
+#1. Create a greeting for your program.
+print("Welcome to Band Name Generator")
+#2. Ask the user for the city that they grew up in.
+user_grewUp_city = input("What's name of the city you grew up in?\n")
+#3. Ask the user for the name of a pet.
+user_pet_name = input("What is name of your pet?\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name could be " + user_grewUp_city + " " +user_pet_name)
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://band-name-generator-end.appbrewery.repl.run/
