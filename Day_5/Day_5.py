21b2f3e5433e5fde1d5789aca1025b1b242c0825
Muhammet Ali Aykanat <72671594+MAAykanat@users.noreply.github.com>
2020-12-10 23:57:02 +0300

    Update before last check

---
+++ Day_5/Day_5.py
@@ -0,0 +1,43 @@
+#Day Number: 4
+#Day Level: Beginner
+#The topic: Python Loops
+
+# Duty: Create a Password Generator
+## Creating a program that can create password as you wish
+
+import random
+
+def printing_password(generated_password):
+    password = ''
+    for _ in range(0,len(generated_password)-1):
+        password = ''.join(generated_password)
+
+    print(password)
+
+def loop_func(list_name,lenth):
+    generated_password = []
+    for _ in range(0,lenth):
+        random_variable = random.randint(0,(len(list_name)-1))
+        generated_password.append(list_name[random_variable])
+    return generated_password
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 
+           's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
+           'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+result_password = []
+
+print("Welcome to the Pypassword Generator!")
+num_letters = int(input("How many letters would you like in you password?"))
+num_symbols = int(input("How many symbols would you like?")) 
+num_digits = int(input("How many digits would you like?"))
+
+result_password.append(loop_func(letters,num_letters))
+result_password.append(loop_func(symbols,num_symbols))
+result_password.append(loop_func(digits,num_digits))
+
+print(result_password)
+printing_password(result_password)
