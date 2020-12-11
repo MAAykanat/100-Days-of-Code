415468dfe9133649eb962abe12bd32d442498407
Muhammet Ali Aykanat <72671594+MAAykanat@users.noreply.github.com>
2020-12-11 22:46:32 +0300

    v1.0.1

--- Day_5/Day_5.py
+++ Day_5/Day_5.py
@@ -24,7 +24,6 @@ def loop_func(list_name,lenth):
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 
            's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
            'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-
 digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -35,7 +34,6 @@ num_letters = int(input("How many letters would you like in you password?"))
 num_symbols = int(input("How many symbols would you like?")) 
 num_digits = int(input("How many digits would you like?"))
 
-result_password = []
 result_password = [*result_password,*(loop_func(letters,num_letters)),*(loop_func(symbols,num_symbols)),*(loop_func(digits,num_digits))]
 
 printing_password(result_password)
